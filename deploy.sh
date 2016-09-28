816af6dc51157878b8c86be3e760c9bd1b85e22d
ZeekHuge <me.zubeen3@gmail.com>
2016-09-28 15:17:12 +0530

    add content

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+echo "Github password for zeekhuge :"
+read -s PASS
 hugo -d ../website
 git add --all
 git commit -m "add content"
-git push origin
+echo -e "zeekhuge\n$PASS" | git push origin 
 cd ../website
 git add --all
 git commit -m "add pages"
