406028059d64cabca57cae4812f9881b7ae33c05
ZeekHuge <me.zubeen3@gmail.com>
2017-04-28 14:46:14 +0530

    add content

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,11 @@
 
 echo "Github password for zeekhuge :"
 read -s PASS
-rm -rf ../website
-mkdir ../website
 hugo -d ../website
 git add --all
 git commit -m "add content"
 git push $(echo "https://zeekhuge:$PASS@github.com/ZeekHuge/zeekhuge.github.io.git") || notify-send "Failed to update the website"
 cd ../website
-git init
 git add --all
 git commit -m "add pages"
 git push -f $(echo "https://zeekhuge:$PASS@github.com/ZeekHuge/zeekhuge.github.io.git") $(echo "master")
